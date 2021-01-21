0aa3f9bd7acfbdffc0c8b3427a073763bcf02332
Mark Sitko <mark@sitko-designing.de>
2021-01-21 12:22:39 +0100

    build flatten promise improvement

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 /*!
- * vue-sanctum v1.0.0
+ * vue-sanctum v1.0.1
  * (c) Mark Sitko <hey@marksitko.de>
  * Released under the MIT License.
  */
@@ -60,62 +60,40 @@ var sanctum = {
 
       var commit = _ref.commit,
           dispatch = _ref.dispatch;
-      return new Promise(function (resolve, reject) {
-        _this._vm.$sanctum.login(credentials).then(function (_ref2) {
-          var data = _ref2.data;
-          commit('updateXSRFTokenState', _this._vm.$sanctum.hasXSRFToken());
-          dispatch('fetchUser');
-          resolve(data);
-        })["catch"](function (error) {
-          return reject(error);
-        });
+      return this._vm.$sanctum.login(credentials).then(function (_ref2) {
+        var data = _ref2.data;
+        commit('updateXSRFTokenState', _this._vm.$sanctum.hasXSRFToken());
+        dispatch('fetchUser');
+        return data;
       });
     },
     fetchUser: function fetchUser(_ref3) {
-      var _this2 = this;
-
       var commit = _ref3.commit;
-      return new Promise(function (resolve, reject) {
-        _this2._vm.$sanctum.me().then(function (_ref4) {
-          var data = _ref4.data;
-          commit('setUser', data);
-          commit('updateAuthenticatedState', true);
-          resolve(data);
-        })["catch"](function (error) {
-          return reject(error);
-        });
+      return this._vm.$sanctum.me().then(function (_ref4) {
+        var data = _ref4.data;
+        commit('setUser', data);
+        commit('updateAuthenticatedState', true);
+        return data;
       });
     },
     logout: function logout(_ref5) {
-      var _this3 = this;
-
       var dispatch = _ref5.dispatch;
-      return new Promise(function (resolve, reject) {
-        _this3._vm.$sanctum.logout().then(function (response) {
-          resolve(response);
-        })["catch"](function (error) {
-          return reject(error);
-        })["finally"](function () {
-          dispatch('clear');
-        });
+      return this._vm.$sanctum.logout()["finally"](function () {
+        dispatch('clear');
       });
     },
     tryAutoLogin: function tryAutoLogin(_ref6) {
       var dispatch = _ref6.dispatch,
           getters = _ref6.getters;
-      return new Promise(function (resolve, reject) {
-        if (!getters.hasXSRFToken) {
-          reject();
-          return;
-        }
 
-        dispatch('fetchUser').then(function (data) {
-          resolve(data);
-        })["catch"](function (error) {
-          //   deleteCookie(COOKIES.XSRF_TOKEN);
-          dispatch('clear');
-          reject(error);
-        });
+      if (!getters.hasXSRFToken) {
+        return Promise.reject();
+      }
+
+      return dispatch('fetchUser')["catch"](function (error) {
+        //   deleteCookie(COOKIES.XSRF_TOKEN);
+        dispatch('clear');
+        throw error;
       });
     },
     clear: function clear(_ref7) {
@@ -178,48 +156,20 @@ var index = {
 
     Vue.prototype.$sanctum = {
       fetchCSRFToken: function fetchCSRFToken() {
-        return new Promise(function (resolve, reject) {
-          axios.get(routes.csrf).then(function (response) {
-            resolve(response);
-          })["catch"](function (error) {
-            return reject(error);
-          });
-        });
+        return axios.get(routes.csrf);
       },
       login: function login(credentials) {
-        var _this = this;
-
-        return new Promise(function (resolve, reject) {
-          _this.fetchCSRFToken().then(function () {
-            axios.post(routes.login, credentials).then(function (response) {
-              resolve(response);
-            })["catch"](function (error) {
-              return reject(error);
-            });
-          })["catch"](function (error) {
-            return reject(error);
-          });
+        return this.fetchCSRFToken().then(function () {
+          return axios.post(routes.login, credentials);
         });
       },
       logout: function logout() {
-        return new Promise(function (resolve, reject) {
-          axios.post(routes.logout).then(function (response) {
-            resolve(response);
-          })["catch"](function (error) {
-            return reject(error);
-          })["finally"](function () {
-            deleteCookie(xsrfToken);
-          });
+        return axios.post(routes.logout)["finally"](function () {
+          deleteCookie(xsrfToken);
         });
       },
       me: function me() {
-        return new Promise(function (resolve, reject) {
-          axios.get(routes.me).then(function (response) {
-            resolve(response);
-          })["catch"](function (error) {
-            return reject(error);
-          });
-        });
+        return axios.get(routes.me);
       },
       hasXSRFToken: function hasXSRFToken() {
         return hasCookie(xsrfToken);
